0fc40ac76c0ed7eaec0c36e3c2da7c437fb303fe
Cat Soup <98920372+CatSoup1@users.noreply.github.com>
2022-07-21 13:49:19 +1000

    init

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+)
+
+
+func main() {
+	url := os.Getenv("url")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(url))
+	if err != nil {
+		panic(err)
+	}
+	col := client.Database("Cluster1").Collection("idle-nought")
+
+	r := gin.Default()
+
+	r.GET("/get/leaderboard", func(c *gin.Context) {
+		cur, err := col.Find(context.TODO(), bson.D{{}})
+		if err != nil {
+			panic(err)
+		}
+		var lbs []primitive.M
+
+		for cur.Next(context.TODO()) {
+			var lb bson.M
+			err := cur.Decode(&lb)
+			if err != nil {
+				panic(err)
+			}
+			lbs = append(lbs, lb)
+		}
+		defer cur.Close(context.TODO())
+
+		c.JSON(200, lb)
+	})
+
+	r.Run(":8080")
+}
+
+)
